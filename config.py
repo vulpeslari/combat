b2c0af26d8a535c5b4704bc07e6f31dd93949014
Larissa de Souza Costa <120164725+vulpeslari@users.noreply.github.com>
2023-01-13 03:40:16 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# Colors
+white = (255, 255, 255)
+yellow = (213, 168, 65)
+blue = (79, 81, 218)
+red = (135, 27, 14)
+green = (127, 220, 41)
+
+# Screen
+sc_width = 1000
+sc_height = 700
+
+# Borders
+border_width = 25
+
+# Score
+score_player1 = 0
+score_player2 = 0
---
+++ main.py
@@ -0,0 +1,105 @@
+import pygame
+from pygame.locals import *
+from config import *
+
+pygame.init()
+
+# Set screen
+screen = pygame.display.set_mode((sc_width, sc_height))
+frames = pygame.time.Clock()
+
+# Drawing Border
+pygame.draw.rect(screen, yellow, (0, 100, sc_width, border_width))
+pygame.draw.rect(screen, yellow, (0, 100, border_width, sc_height - 100))
+pygame.draw.rect(screen, yellow, (0, sc_height - border_width, sc_width, border_width))
+pygame.draw.rect(screen, yellow, (sc_width - border_width, 100, border_width, sc_height - 100))
+
+# Drawing other elements
+pygame.draw.rect(screen, yellow, (250, (sc_height - 100) // 2 + 100, border_width, 50))
+
+# Drawing tanks
+tank1 = pygame.image.load("tank1.png")
+tank2 = pygame.image.load("tank2.png")
+
+# Font
+font = pygame.font.Font("PressStart2P.ttf", 80)
+
+while True:
+    # Redrawing the scenario
+    pygame.draw.rect(screen, yellow, (0, 100, sc_width, border_width))
+    pygame.draw.rect(screen, yellow, (0, 100, border_width, sc_height - 100))
+    pygame.draw.rect(screen, yellow, (0, sc_height - border_width, sc_width, border_width))
+    pygame.draw.rect(screen, yellow, (sc_width - border_width, 100, border_width, sc_height - 100))
+
+    pygame.draw.rect(screen, yellow,
+                     (160, ((sc_height - 225) // 2 + 100) - border_width, border_width * 1.2, border_width * 7))
+    pygame.draw.rect(screen, yellow,
+                     (135, ((sc_height - 225) // 2 + 100) - border_width, border_width, border_width))
+    pygame.draw.rect(screen, yellow,
+                     (135, ((sc_height + 75) // 2 + 100) - border_width, border_width, border_width))
+
+    pygame.draw.rect(screen, yellow,
+                     (830, ((sc_height - 225) // 2 + 100) - border_width, border_width * 1.2, border_width * 7))
+    pygame.draw.rect(screen, yellow,
+                     (855, ((sc_height - 225) // 2 + 100) - border_width, border_width, border_width))
+    pygame.draw.rect(screen, yellow,
+                     (855, ((sc_height + 75) // 2 + 100) - border_width, border_width, border_width))
+
+    pygame.draw.rect(screen, yellow,
+                     (160, ((sc_height - 440) // 2 + 100) - border_width, border_width * 3.4, border_width * 1.15))
+    pygame.draw.rect(screen, yellow,
+                     (160, ((sc_height + 300) // 2 + 100) - border_width, border_width * 3.4, border_width * 1.15))
+    pygame.draw.rect(screen, yellow,
+                     (780, ((sc_height - 440) // 2 + 100) - border_width, border_width * 3.4, border_width * 1.15))
+    pygame.draw.rect(screen, yellow,
+                     (780, ((sc_height + 300) // 2 + 100) - border_width, border_width * 3.4, border_width * 1.15))
+
+    pygame.draw.rect(screen, yellow,
+                     (250, ((sc_height - 100) // 2 + 100) - border_width, border_width * 2, border_width * 2))
+    pygame.draw.rect(screen, yellow,
+                     (740 - border_width, ((sc_height - 100) // 2 + 100) - border_width, border_width * 2,
+                      border_width * 2))
+
+    pygame.draw.rect(screen, yellow,
+                     (350, ((sc_height - 350) // 2 + 100) - border_width, border_width * 3, border_width))
+    pygame.draw.rect(screen, yellow,
+                     (350, ((sc_height - 300) // 2 + 100) - border_width, border_width, border_width * 1))
+    pygame.draw.rect(screen, yellow,
+                     (350, ((sc_height + 200) // 2 + 100) - border_width, border_width * 3, border_width))
+    pygame.draw.rect(screen, yellow,
+                     (350, ((sc_height + 150) // 2 + 100) - border_width, border_width, border_width * 1))
+    pygame.draw.rect(screen, yellow,
+                     (590, ((sc_height - 350) // 2 + 100) - border_width, border_width * 3, border_width))
+    pygame.draw.rect(screen, yellow,
+                     (640, ((sc_height - 300) // 2 + 100) - border_width, border_width, border_width * 1))
+    pygame.draw.rect(screen, yellow,
+                     (590, ((sc_height + 200) // 2 + 100) - border_width, border_width * 3, border_width))
+    pygame.draw.rect(screen, yellow,
+                     (640, ((sc_height + 150) // 2 + 100) - border_width, border_width, border_width * 1))
+
+    pygame.draw.rect(screen, yellow,
+                     (480, (sc_height - 50) - border_width, border_width * 2, border_width * 2))
+    pygame.draw.rect(screen, yellow,
+                     (480, (sc_height - 550) - border_width, border_width * 2, border_width * 2))
+
+    # Updating tanks
+    screen.blit(tank1, (55, 330))
+    screen.blit(tank2, (815, 330))
+
+    # Drawing the score
+    score_format = font.render("{}".format(score_player1), False, green)
+    screen.blit(score_format, (225, 10))
+
+    score_format = font.render("{}".format(score_player2), False, blue)
+    screen.blit(score_format, (725, 10))
+
+    for event in pygame.event.get():
+        # Quits if you close the window
+        if event.type == QUIT:
+            pygame.display.quit()
+            exit()
+
+    # Screen in loop
+    frames.tick(200)
+    pygame.display.update()
+    screen.fill(red)
