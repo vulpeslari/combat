e9843b1578d866e12087acb46827536669efc5c5
Larissa de Souza Costa <120164725+vulpeslari@users.noreply.github.com>
2023-01-15 20:07:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,16 @@ pygame.draw.rect(screen, yellow, (sc_width - border_width, 100, border_width, sc
 # Drawing other elements
 pygame.draw.rect(screen, yellow, (250, (sc_height - 100) // 2 + 100, border_width, 50))
 
-# Drawing tanks
-tank1 = pygame.image.load("tank1.png")
-tank1_rect = tank1.get_rect()
-tank2 = pygame.image.load("tank2.png")
-tank2_rect = tank2.get_rect()
+# Setting sprites
+drawGroup = pygame.sprite.Group()
+tank1 = pygame.sprite.Sprite(drawGroup)
+tank2 = pygame.sprite.Sprite(drawGroup)
+
+# Drawing Tanks
+tank1.image = pygame.image.load("tank1.png")
+tank2.image = pygame.image.load("tank2.png")
+tank1.rect = pygame.rect.Rect(55, 330, 0, 0)
+tank2.rect = pygame.rect.Rect(815, 330, 0, 0)
 
 # Font
 font = pygame.font.Font("PressStart2P.ttf", 80)
@@ -85,10 +90,11 @@ while True:
                      (480, (sc_height - 550) - border_width, border_width * 2, border_width * 2))
 
     # Updating tanks
-    rotated_tank1 = pygame.transform.rotate(tank1, angle)
+    rotated_tank1 = pygame.transform.rotate(tank1.image, angle1)
+    rotated_tank2 = pygame.transform.rotate(tank2.image, angle2)
 
-    screen.blit(rotated_tank1, tank1_position)
-    screen.blit(tank2, tank2_position)
+    screen.blit(rotated_tank1, (tank1.rect.x, tank1.rect.y))
+    screen.blit(rotated_tank2, (tank2.rect.x, tank2.rect.y))
 
     # Drawing the score
     score_format = font.render("{}".format(score_player1), False, green)
@@ -103,24 +109,61 @@ while True:
             pygame.display.quit()
             exit()
 
+    # Keyboard movements
         key = pygame.key.get_pressed()
         if key[pygame.K_w]:
-            if tank1_position[0] >= sc_width - border_width * 4:
-                tank1_position[0] = tank1_position[0]
+            if tank1.rect.x >= sc_width - border_width * 4:
+                tank1.rect.x = tank1.rect.x
             else:
-                tank1_position[0] += 1
+                tank1.rect.x += 1
+            if angle1 > 0:
+                tank1.rect.y -= 0.5
+            if angle1 < 0:
+                tank1.rect.y += 1
 
         if key[pygame.K_a]:
-            angle += 1
+            angle1 += 0.5
+            tank1.rect.x -= 0.5
+
+        if key[pygame.K_d]:
+            angle1 -= 0.5
+            tank1.rect.x -= 0.3
 
         if key[pygame.K_UP]:
-            if tank2_position[0] <= 0 - border_width * 1.5:
-                tank2_position[0] = tank2_position[0]
+            if tank2.rect.x <= 0 - border_width * 4:
+                tank2.rect.x = tank2.rect.x
             else:
-                tank2_position[0] -= 1
+                tank2.rect.x -= 1
+            if angle2 > 0:
+                tank2.rect.y += 1
+            if angle2 < 0:
+                tank2.rect.y -= 1
+
+        if key[pygame.K_RIGHT]:
+            angle2 -= 0.5
+            tank1.rect.x -= 0.3
 
         if key[pygame.K_LEFT]:
-            pygame.transform.flip(tank2, 100, 100)
+            angle2 += 0.5
+            tank2.rect.x -= 0.3
+
+    # Collision with the walls
+    def collide(obj):
+        if obj.rect.x <= -30:
+            obj.rect.x = obj.rect.x + 5
+
+        if obj.rect.y >= 585:
+            tank1.rect.y = tank1.rect.y - 5
+
+        if obj.rect.y <= 70:
+            obj.rect.y = obj.rect.y + 5
+
+        if obj.rect.x >= 890:
+            obj.rect.x = obj.rect.x - 5
+
+    collide(tank1)
+    collide(tank2)
+
 
     # Screen in loop
     frames.tick(200)
